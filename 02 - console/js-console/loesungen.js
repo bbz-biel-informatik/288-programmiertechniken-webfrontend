9ccf7b15fe13d65aae8b40c9889134942839df0a
Nicolas Müller <nicolas.mueller2@gmail.com>
2025-11-18 11:59:02 +0100

    add solutions file

---
+++ 02 - console/js-console/loesungen.js
@@ -0,0 +1,7 @@
+// Hier können Lösungen für verschiedene Aufgaben gespeichert werden.
+// Beispiel: Aufgabe 1
+let text = await getInput("Geben Sie einen Text ein:");
+print("Eingegebener Text: " + text);
+
+// Aufgabe 2: Zahlen addieren
+// ...
