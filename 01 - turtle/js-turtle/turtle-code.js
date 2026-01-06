70e0089123e3e361d96cf95d8b9f869d5d1f51b2
Nicolas Müller <nicolas.mueller2@gmail.com>
2026-01-06 07:45:20 +0100

    add llms.txt

--- 01 - turtle/js-turtle/turtle-code.js
+++ 01 - turtle/js-turtle/turtle-code.js
@@ -1,3 +1,17 @@
 function start(turtle) {
   turtle.setSpeed(1);
 }
+
+let spieler = document.querySelector("#player");
+
+function loop() {
+  // Collision check
+  let obstacles = document.querySelectorAll(".obstacle"); // Das sind die Hindernisse zum beispiel.
+  if (anyCollision(spieler, obstacles)) {
+    alert("Game over!");
+    return;
+  }
+  window.requestAnimationFrame(loop);
+}
+
+window.requestAnimationFrame(loop);
